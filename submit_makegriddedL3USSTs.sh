47897149baee7dea97f90964fa072c1e7406c12d
Charles Roberts <c.j.roberts@reading.ac.uk>
2021-03-05 11:32:33 +0000

    Changed conda environment in submit_makegriddedL3USSTs.sh.

--- submit_makegriddedL3USSTs.sh
+++ submit_makegriddedL3USSTs.sh
@@ -4,5 +4,5 @@
 #SBATCH -e makegriddedL3USSTs_%j.err
 #SBATCH --time=24:00:00
 
-conda activate cci
+conda activate regridding_tools
 python ./makegriddedL3USSTs.py 5 5 monthly AVHRR07_G --year 1982 --sst_l3u_path /neodc/esacci/sst/data/CDR_v2/AVHRR/L3U/v2.1 --out_path /gws/nopw/j04/esacci_sst/output/l3_regrid/CDR2.1
