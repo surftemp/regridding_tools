fd27cef5f5c69432f2dcc60b22c9fa4876743574
Charles Roberts <c.j.roberts@reading.ac.uk>
2021-03-18 16:28:11 +0000

    Time series plots.

--- plotting/compare_L3_Had.py
+++ plotting/compare_L3_Had.py
@@ -25,9 +25,9 @@ outPicPath = "/Users/charles/Data/HadSSTComps/Plots"
 titlestr = 'N07'
 
 
-def compare_L3_Had(hadfiles, l3path, l4path, outPicPath, titlestr="", umax=0.35, xbins=80, xrange=(-2.5, 2.5)):
+def compare_L3_Had(hadfiles, l3path, l4path, outPicPath, titlestr='', umax=0.35, xbins=80, xrange=(-2.5, 2.5)):
     """
-    Compare regridded L3U data to HadSST.
+    Compare regridded L3U data to L4 and HadSST.
 
     :param hadfiles:
     :param l3path:
@@ -39,8 +39,7 @@ def compare_L3_Had(hadfiles, l3path, l4path, outPicPath, titlestr="", umax=0.35,
     :param xrange:
     :return:
     """
-    # Strip whitespace from either side of the title string.
-    titlestr = titlestr.strip()
+    titlestr = format_titlestr(titlestr)
 
     # Read the L3 data regridded to 5 degree monthly
     dsl3 = xr.open_mfdataset(os.path.join(l3path, '*.nc'), combine='by_coords')
@@ -86,33 +85,11 @@ def compare_L3_Had(hadfiles, l3path, l4path, outPicPath, titlestr="", umax=0.35,
 
     # Compare the  new regridded l3 and the previous l4
     diff = dsl3.sst - dsl4.sst
-    plot_maps_and_hists(diff, titlestr + ' L3 - L4', 'l3-l4', outPicPath, xbins, xrange, vmax=0.5)
+    create_plots(diff, titlestr + 'L3 - L4', 'l3-l4', outPicPath, xbins, xrange, vmax=0.5)
 
     # Compare the l3 and HadSST4
     diff = dsl3.sst - dsH.sst
-    plot_maps_and_hists(diff, titlestr + ' L3 - HadSST', 'l3-had', outPicPath, xbins, xrange)
-
-    # Timeseries of statistics
-    # First, using all common data
-    dt = (dsl3.sst - dsl4.sst).mean(dim=('lat', 'lon'))
-    sd = (dsl3.sst - dsl4.sst).std(dim=('lat', 'lon'))
-    dtu = dt + sd
-    dtl = dt - sd
-    dt.plot(color='black')
-    dtu.plot(color='red')
-    dtl.plot(color='red')
-    plt.title(titlestr + "L3 - L4, global mean +/- 1 sig")
-    plt.show()
-
-    dt = (dsl3.sst - dsH.sst).mean(dim=('lat', 'lon'))
-    sd = (dsl3.sst - dsH.sst).std(dim=('lat', 'lon'))
-    dtu = dt + sd
-    dtl = dt - sd
-    dt.plot(color='black')
-    dtu.plot(color='red')
-    dtl.plot(color='red')
-    plt.title(titlestr + "L3 - Had4, all-data mean +/- 1 sig")
-    plt.show()
+    create_plots(diff, titlestr + 'L3 - HadSST', 'l3-had ', outPicPath, xbins, xrange)
 
     # Then the same things using only the low-uncertainty data in HadSST4
     # Mask the high uncertainty HadSST4
@@ -121,48 +98,32 @@ def compare_L3_Had(hadfiles, l3path, l4path, outPicPath, titlestr="", umax=0.35,
 
     # Compare the l3 and HadSST4 best data in HadSST4
     diff = dsl3.sst - dsHg.sst
-    plot_maps_and_hists(diff, titlestr + ' L3 - HadSST Best Data', 'l3-had_best', outPicPath, xbins, xrange, vmax=1.0)
-
-    # using best data
-    # add 1% and 99% quantiles, and median
-    dt = (dsl3.sst - dsHg.sst).mean(dim=('lat', 'lon'))
-    sd = (dsl3.sst - dsHg.sst).std(dim=('lat', 'lon'))
-    dtu = dt + sd
-    dtl = dt - sd
-    med = (dsl3.sst - dsHg.sst).median(dim=('lat', 'lon'))
-    q1 = (dsl3.sst - dsHg.sst).quantile(0.01, dim=('lat', 'lon'))
-    q99 = (dsl3.sst - dsHg.sst).quantile(0.99, dim=('lat', 'lon'))
-
-    q1.plot(color='orange', label='Q01')
-    dtu.plot(color='red', label='+1 sig')
-    dt.plot(color='black', label='mean')
-    med.plot(color='darkgrey', label='median')
-    dtl.plot(color='red', label='-1 sig')
-    q99.plot(color='orange', label='Q99')
-    plt.title(titlestr + "L3 - Had4, best in-situ mean +/- 1 sig")
-    plt.legend()
-    plt.show()
+    create_plots(diff, titlestr + 'L3 - HadSST Best Data', 'l3-had_best', outPicPath, xbins, xrange, vmax=1.0)
 
 
-def plot_maps_and_hists(diff, titlestr, filestr, outPicPath, xbins, xrange, vmin=None, vmax=None):
+def create_plots(diff, titlestr, filestr, outPicPath, xbins, xrange, vmin=None, vmax=None):
     """
-    Plot maps and histograms.
+    Create maps, histograms and time series of statistics for a particular difference between two datasets.
 
     :param diff:
     :param titlestr:
     :param filestr:
     :param outPicPath:
-    :param vmin:
-    :param vmax:
     :param xbins:
     :param xrange:
+    :param vmin:
+    :param vmax:
     :return:
     """
+    titlestr = format_titlestr(titlestr)
+
+    # Plot maps and histograms
     yearly_diffs = diff.groupby('time.year')
     for year, yearly_diff in yearly_diffs:
         time = yearly_diff.time
         time_size = time.size
 
+        # Panel plots of maps for each month for each year
         if time_size == 1:
             p = yearly_diff.plot(vmin=vmin, vmax=vmax,
                                  transform=ccrs.PlateCarree(), subplot_kws=dict(projection=ccrs.PlateCarree()))
@@ -175,10 +136,11 @@ def plot_maps_and_hists(diff, titlestr, filestr, outPicPath, xbins, xrange, vmin
             for ax in p.axes.flat:
                 ax.coastlines()
 
-        plt.suptitle(titlestr + ' Maps for ' + str(year))
-        plt.savefig(os.path.join(outPicPath, str(year) + '_' + filestr + '_maps.pdf'))
+        plt.suptitle(titlestr + 'Maps for ' + str(year))
+        plt.savefig(os.path.join(outPicPath, filestr + '_maps_' + str(year) + '.pdf'))
         plt.close()
 
+        # The same thing for histograms
         nrows = (time_size - 1) // 4 + 1
         ncols = time_size if time_size <= 4 else 4
         fig, axs = plt.subplots(nrows, ncols, figsize=(6.4 * ncols, 4.8 * nrows),
@@ -196,24 +158,50 @@ def plot_maps_and_hists(diff, titlestr, filestr, outPicPath, xbins, xrange, vmin
                          transform=ax.transAxes)
             else:
                 ax.set_axis_off()
-        plt.suptitle(titlestr + ' Histograms for ' + str(year))
-        plt.savefig(os.path.join(outPicPath, str(year) + '_' + filestr + '_hists.pdf'))
+        plt.suptitle(titlestr + 'Histograms for ' + str(year))
+        plt.savefig(os.path.join(outPicPath, filestr + '_hists_' + str(year) + '.pdf'))
         plt.close()
 
         # Whole series plots, all data
         p = yearly_diff.mean(dim='time').plot(vmin=vmin, vmax=vmax, transform=ccrs.PlateCarree(),
                                               subplot_kws=dict(projection=ccrs.PlateCarree()))
         p.axes.coastlines()
-        plt.suptitle(titlestr + ' Annual Mean Map for ' + str(year))
-        plt.savefig(os.path.join(outPicPath, str(year) + '_' + filestr + '_mean_map.pdf'))
+        plt.suptitle(titlestr + 'Annual Mean Map for ' + str(year))
+        plt.savefig(os.path.join(outPicPath, filestr + '_mean_map_' + str(year) + '.pdf'))
         plt.close()
 
         yearly_diff.plot.hist(bins=xbins, range=xrange)
         coeff = add_gaussian(yearly_diff, plt.gca(), xbins, xrange)
         plt.text(0.75, 0.8, '$a = {0:.3f}$\n$\mu = {1:.3f}$\n$\sigma = {2:.3f}$'.format(*coeff), transform=ax.transAxes)
-        plt.suptitle(titlestr + ' Whole Series Histogram for ' + str(year))
-        plt.savefig(os.path.join(outPicPath, str(year) + '_' + filestr + '_whole_series_hist.pdf'))
+        plt.suptitle(titlestr + 'Whole Series Histogram for ' + str(year))
+        plt.savefig(os.path.join(outPicPath, filestr + '_whole_series_hist_' + str(year) + '.pdf'))
         plt.close()
+
+    # Time series of statistics
+    dt = diff.mean(dim=('lat', 'lon'))
+    sd = diff.std(dim=('lat', 'lon'))
+    dtu = dt + sd
+    dtl = dt - sd
+    med = diff.median(dim=('lat', 'lon'))
+    q1 = diff.quantile(0.01, dim=('lat', 'lon'))
+    q99 = diff.quantile(0.99, dim=('lat', 'lon'))
+
+    q1.plot(color='orange', label='Q01')
+    dtu.plot(color='red', label='+1 sig')
+    dt.plot(color='black', label='mean')
+    med.plot(color='darkgrey', label='median')
+    dtl.plot(color='red', label='-1 sig')
+    q99.plot(color='orange', label='Q99')
+    plt.title(titlestr + 'Mean and Median with +/- 1 Sig, 1st and 99th Percentile')
+    plt.legend()
+    plt.savefig(os.path.join(outPicPath, filestr + '_time_series.pdf'))
+    plt.close()
+
+
+def format_titlestr(titlestr):
+    """Strip whitespace from either side of the title string and add a single space."""
+    titlestr = titlestr.strip() + ' '
+    return titlestr
 
 
 def add_gaussian(data, ax, xbins, xrange):
