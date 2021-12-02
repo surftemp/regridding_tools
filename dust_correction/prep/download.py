3fab88c0dc328fceb1856c832920af3614dfcbba
Niall McCarroll <niallmcc@host425.jc.rl.ac.uk>
2021-12-02 09:36:47 +0000

    updates

--- dust_correction/prep/download.py
+++ dust_correction/prep/download.py
@@ -6,13 +6,13 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--start-year", type=int, default=2020)
-parser.add_argument("--end-year", type=int, default=2021)
+parser.add_argument("--end-year", type=int, default=2020)
 
 args = parser.parse_args()
 
 c = cdsapi.Client(progress=False)
 
-for year in range(args.start_year, args.end_year):
+for year in range(args.start_year, args.end_year+1):
     try:
         c.retrieve(
             'cams-global-reanalysis-eac4',
