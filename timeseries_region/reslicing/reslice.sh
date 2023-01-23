1aff7463bde2020f9a5343f9bd6be66c051eac21
Niall McCarroll <niall_mccarroll@yahoo.com>
2023-01-23 10:28:09 +0000

    adjust file paths as locations are different in new server

--- timeseries_region/reslicing/reslice.sh
+++ timeseries_region/reslicing/reslice.sh
@@ -10,5 +10,5 @@ ENDYEAR=$2
 for ((YEAR=$STARTYEAR;YEAR<=$ENDYEAR;YEAR+=1))
 do
     echo running reslice job for $YEAR
-    python reslice.py --input-cci /data/input/CDR_v2/Analysis/L4/v2.1/ --input-c3s /data/input/ICDR_v2/Analysis/L4/v2.0/ --input-climatology /data/input/CDR_v2/Climatology/L4/v2.1/ --output-folder /data/input/reslice --year $YEAR
+    python reslice.py --input-cci /data/data1/input/CDR_v2/Analysis/L4/v2.1/ --input-c3s /data/data1/input/ICDR_v2/Analysis/L4/v2.0/ --input-climatology /data/data1/input/CDR_v2/Climatology/L4/v2.1/ --output-folder /data/data1/input/reslice --year $YEAR
 done
--- timeseries_region/reslicing/reslice_dust.sh
+++ timeseries_region/reslicing/reslice_dust.sh
@@ -10,5 +10,5 @@ ENDYEAR=$2
 for ((YEAR=$STARTYEAR;YEAR<=$ENDYEAR;YEAR+=1))
 do
     echo running reslice job for $YEAR
-    python reslice.py --input-cci /data2/input/dust/CDR_v2/Analysis/L4/v2.1/ --input-c3s /data2/input/dust/ICDR_v2/Analysis/L4/v2.0/ --input-climatology /data2/input/dust/CDR_v2/Climatology/L4/v2.1/ --output-folder /data2/input/dust/reslice --year $YEAR
+    python reslice.py --input-cci /data/data2/input/CDR_v2/Analysis/L4/v2.1/ --input-c3s /data/data2/input/ICDR_v2/Analysis/L4/v2.0/ --input-climatology /data/data2/input/CDR_v2/Climatology/L4/v2.1/ --output-folder /data/data2/input/reslice --year $YEAR
 done
